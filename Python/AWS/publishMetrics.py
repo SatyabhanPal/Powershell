78befce0bfa8ba27c74d2f9510bf56b46e08ac58
John Doe <john@sample.com>
2017-01-21 17:27:08 +0530

    Updated the file for memory publish

--- Python/AWS/publishMetrics.py
+++ Python/AWS/publishMetrics.py
@@ -1,30 +1,51 @@
 import boto3
 import datetime
-
+import psutil
+import time
+import platform
+import requests
 client = boto3.client(
     'cloudwatch',
     # Hard coded strings as credentials, not recommended.
-    aws_access_key_id='AKIAIRLTODWZYZPJCCTA',
-    aws_secret_access_key='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'
+    aws_access_key_id='',
+    aws_secret_access_key='',
+    region_name='us-west-2'
 )
-print(client.list_metrics())
+#print(client.list_metrics())
 d=datetime.datetime.now()
 print(d)
-response=client.put_metric_data(
-    Namespace='windows',
-    MetricData=[
-        {
-            'MetricName': 'MemoryUtilization',
-            'Dimensions': [
-                {
-                    'Name': 'instanceid',
-                    'Value': 'i-0af25281fa3a80b3f'
-                },
-            ],
-            'Timestamp': (datetime.datetime.utcnow()),
-            'Value': 50.0,
-            'Unit': 'Percent'
-        },
-    ]
-)
-print(response)
+res=requests.get("http://169.254.169.254/latest/meta-data/instance-id")
+instanceid=res.content
+print(instanceid)
+def publishMetrics(namespace,metricname,instanceid,value,unit):
+        print(instanceid)
+        response=client.put_metric_data(
+                Namespace=namespace,
+                MetricData=[
+                        {
+                                'MetricName': metricname,
+                                'Dimensions': [
+                                        {
+                                                'Name': 'instanceid',
+                                                'Value': instanceid
+                                        },
+                                ],
+                                'Timestamp': (datetime.datetime.utcnow()),
+                                'Value': value,
+                                'Unit': unit
+                        },
+                ]
+        )
+while(1):
+        print("Publishing metrics")
+        usedmemory=psutil.virtual_memory().used/(1024*1024)
+        print(usedmemory)
+        totalmemory=psutil.virtual_memory().total/(1024*1024)
+        print(totalmemory)
+        utilization=float(usedmemory)/float(totalmemory)*100
+        avilable=totalmemory-usedmemory
+        print("Utilization value is :%s" %(utilization))
+        print("Available memory is :%s MB" %(avilable))
+        publishMetrics(platform.system(),"MemoryAvailable",instanceid,avilable,"Megabytes")
+        publishMetrics(platform.system(),"MemoryUtilization",instanceid,utilization,"Percent")
+        time.sleep(5)
